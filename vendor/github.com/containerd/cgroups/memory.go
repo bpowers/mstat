9ffacbab8bb5f1b39440c4772625b4797193286f
Bobby Powers <bobbypowers@gmail.com>
2017-11-12 19:54:56 -0500

    build: update vendored libs

--- vendor/github.com/containerd/cgroups/memory.go
+++ vendor/github.com/containerd/cgroups/memory.go
@@ -144,7 +144,7 @@ func (m *memoryController) Stat(path string, stats *Metrics) error {
 			}
 			parts = append(parts, tt.name)
 			v, err := readUint(filepath.Join(m.Path(path), strings.Join(parts, ".")))
-			if err != nil && t.module != "memsw" {
+			if err != nil {
 				return err
 			}
 			*tt.value = v
@@ -160,7 +160,7 @@ func (m *memoryController) OOMEventFD(path string) (uintptr, error) {
 		return 0, err
 	}
 	defer f.Close()
-	fd, _, serr := unix.RawSyscall(unix.SYS_EVENTFD2, 0, unix.FD_CLOEXEC, 0)
+	fd, _, serr := unix.RawSyscall(unix.SYS_EVENTFD2, 0, unix.EFD_CLOEXEC, 0)
 	if serr != 0 {
 		return 0, serr
 	}
