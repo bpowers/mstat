819d2cba8b08362508a0a76c359fd75798374a7b
Bobby Powers <bobbypowers@gmail.com>
2018-11-04 22:45:01 -0800

    also record kernel memory use

--- main.go
+++ main.go
@@ -148,11 +148,12 @@ func main() {
 
 	cgroupPath := cgroups.StaticPath(newPath())
 
-	memLimit := int64(4 * 1024 * 1024 * 1024) // 4 GB
+	memLimit := int64(8 * 1024 * 1024 * 1024) // 4 GB
 	cgroup, err := cgroups.New(cgroups.V1, cgroupPath, &specs.LinuxResources{
 		//CPU: &specs.LinuxCPU{},
 		Memory: &specs.LinuxMemory{
-			Limit: &memLimit,
+			Limit:  &memLimit,
+			Kernel: &memLimit,
 		},
 	})
 	if err != nil {
@@ -234,14 +235,14 @@ func main() {
 
 	var buf bytes.Buffer
 	bio := bufio.NewWriter(&buf)
-	if _, err := bio.WriteString("time\trss\n"); err != nil {
+	if _, err := bio.WriteString("time\trss\tkernel\n"); err != nil {
 		log.Fatalf("bio.WriteString: %s", err)
 	}
 
 	start := stats.Rss[0].Time.UnixNano()
 	for i := 0; i < len(stats.Rss); i++ {
 		r := stats.Rss[i]
-		line := fmt.Sprintf("%d\t%d\n", r.Time.UnixNano()-start, r.Value)
+		line := fmt.Sprintf("%d\t%d\t%d\n", r.Time.UnixNano()-start, r.Value, r.Kernel)
 		if _, err := bio.WriteString(line); err != nil {
 			log.Fatalf("bufio.WriteString: %s", err)
 		}
